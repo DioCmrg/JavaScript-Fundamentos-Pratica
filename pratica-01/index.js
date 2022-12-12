6a7a3f24d67890b5cbf8f8f6230cf79cd1968155
Diovane <111087516+DioCmrg@users.noreply.github.com>
2022-12-12 01:16:14 +0000

    Usando prompt-sync - pratica 02

--- pratica-01/index.js
+++ pratica-01/index.js
@@ -1,5 +1,4 @@
-var salario_bruto = 7500;
-var ir = salario_bruto * 0.10;
-var salario_liquido = salario_bruto - ir;
-console.log("Salário líquido: R$",
-  salario_liquido);
+let salario_bruto = 7500;
+let ir = salario_bruto * 0.10;
+let salario_liquido = salario_bruto - ir;
+console.log("Salário líquido: R$", salario_liquido);
