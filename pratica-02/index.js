da41a905ae98eaf9db9e688f3f9b3420f8e59f30
Diovane <111087516+DioCmrg@users.noreply.github.com>
2022-12-18 14:29:49 +0000

    07 - Comandos de Decisão

--- pratica-02/index.js
+++ pratica-02/index.js
@@ -1,7 +1,7 @@
 const prompt = require("prompt-sync")();
 
 let nome = prompt("Digite seu nome: ");
-let salario_bruto = parseFloat(prompt("Digite seu nome: "));
+let salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
 let ir = salario_bruto * 0.10;
 let salario_liquido = salario_bruto - ir;
 console.log("Salário líquido: R$", salario_liquido);
